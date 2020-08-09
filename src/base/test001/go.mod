045ebbc721bfe2365f739d33d2868a65c9d5f7f5
luoyusnnu <2482840559@qq.com>
2020-08-09 22:20:18 +0800

    代码优化

---
+++ src/base/test001/go.mod
@@ -0,0 +1,3 @@
+module github.com/sahara-gopher/tour/src/base/test001
+
+go 1.14
--- src/base/test001/main.go
+++ src/base/test001/main.go
@@ -1,5 +1,42 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"sync"
+)
 
+func main() {
+	var wg sync.WaitGroup
+	chd := make(chan bool, 100)
+	cha := make(chan bool, 100)
+	var digestArr = []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	var charArr = "abcdefghigk"
+	wg.Add(2)
+	go func() {
+		defer func() {
+			wg.Done()
+		}()
+		for _, v := range digestArr {
+			fmt.Println("----")
+			<-chd
+			fmt.Println(v)
+			cha <- true
+		}
+		close(cha)
+	}()
+
+	go func() {
+		defer func() {
+			wg.Done()
+		}()
+		for _, v := range []byte(charArr) {
+			<-cha
+			fmt.Println(string(v))
+			chd <- true
+		}
+		close(chd)
+	}()
+	chd <- true
+	wg.Wait()
+	fmt.Println("Done")
 }
